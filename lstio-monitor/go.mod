ae2dc7e62bce8365bdf066fba8181fc2e631bb2e
Administrator <3040357205@qq.com>
2023-11-24 21:57:19 +0800

    feat(product_cpu_used): 监控器生成CPU使用率 在lstio-monitor项目中下载CPU_used.csv文件并使用go-echarts包将数据可视化并生成table表格。

--- lstio-monitor/go.mod
+++ lstio-monitor/go.mod
@@ -1,3 +1,5 @@
 module lstio-monitor
 
 go 1.21
+
+require github.com/go-echarts/go-echarts v1.0.0
---
+++ lstio-monitor/src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"lstio-monitor/dataset"
+)
+
+func main() {
+	dataset.ProductCPUTable() // 生成CPU使用率图表
+}
